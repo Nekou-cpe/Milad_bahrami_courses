f3e28ad18581c6fe37ca9a0c70df539628bd18d0
Nekou-cpe <Nekou.workcpe@gmail.com>
2024-08-12 23:31:34 +0330

    day 2 update

---
+++ js/38-45/script.js
@@ -0,0 +1,5 @@
+let y=0;
+for (let x = 0; x < 10; x++) {
+        y+=x        
+}
+console.log(y);
--- js/55-64/javaScript.js
+++ js/55-64/javaScript.js
@@ -1,44 +1,6 @@
-let str1='',str2='',str3='',str4='';
-
-for (let x = 1; x < 6; x++) {
-    for (let i = 1; i < x+1; i++) {
-       
-        str1+='*';
-        
-    }
-    str1+='\n';
+const cars=['BMW','Mercedes','Benz'];
+const numbers=new Array(1,2,3);
+for (let i = 4; i <10; i++){
+    numbers[i]='bt'+i;
 }
-for (let y = 5; y>0; y--) {
-    for (let j = y; j >0; j--) {
-        
-        str2+='*';
-    }
-    str2+='\n';
-    
-}
-for (let x = 1; x < 6; x++) {
-    for (let j = x; j<5-x; j++) {
-        str3+=' ';
-        
-       }
-    for (let i = 1; i < x+1; i++) {
-       
-        str3+='*';
-        
-    }
-    str3+='\n';
-}
-for (let y = 5; y>0; y--) {
-    for (let j= 5-y; j>0; j--) {
-        str4+=' ';
-        
-       }
-    for (let j = y; j >0; j--) {
-        
-        str4+='*';
-    }
-    str4+='\n';
-    
-}
-
-console.log(str1+'\n'+str2+'\n'+str3+'\n'+str4);
+console.log(cars)
