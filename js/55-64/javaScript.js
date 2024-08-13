b3aac23febaf486327809d5f26db3bd349a181be
Nekou-cpe <Nekou.workcpe@gmail.com>
2024-08-13 21:31:09 +0330

    update 9

--- js/55-64/javaScript.js
+++ js/55-64/javaScript.js
@@ -1,6 +1,34 @@
-const cars=['BMW','Mercedes','Benz'];
-const numbers=new Array(1,2,3);
-for (let i = 4; i <10; i++){
-    numbers[i]='bt'+i;
+/*
+Basic Array Methods
+Array length
+Array toString()
+Array at()->cars[0]
+Array join() -> str+' '
+Array pop()-> remove last element
+Array push()
+
+See Also:
+Search Methods
+Sort Methods
+Iteration Methods	Array shift() ->remove first element
+Array unshift() ->add new element from first
+Array delete() ->dont use it
+Array concat() ->stick two arrays to gether
+Array copyWithin()
+Array flat()
+Array splice()->remove some elements
+Array toSpliced()
+Array slice()
+*/
+const cars=[];
+cars[0]='bmw';
+cars[1]='benz';
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+document.getElementById("demo").innerHTML = "First number over 18 is " + first;
+
+function myFunction(value, index, array) {
+  return value > 18;
 }
-console.log(cars)
+console.log(cars.join('*')+ ' ' + cars.length);
